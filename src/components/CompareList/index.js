8cb46511fad65425a0d72321b128114f1aa472ed
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-17 17:09:26 -0300

    desafio completo

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
-const CompareList = ({ repositories }) => (
+const CompareList = ({ repositories, removeRepository, updateRepository }) => (
   <Container>
     {repositories.map(repository => (
       <Repository key={repository.id}>
@@ -35,6 +35,16 @@ const CompareList = ({ repositories }) => (
             <small>last commit</small>
           </li>
         </ul>
+        <div className="buttons-container">
+          <button type="button" onClick={() => updateRepository(repository.id)}>
+            <i className="fa fa-retweet" />
+            Atualizar
+          </button>
+          <button type="button" onClick={() => removeRepository(repository.id)}>
+            <i className="fa fa-trash" />
+            Excluir
+          </button>
+        </div>
       </Repository>
     ))}
   </Container>
@@ -55,6 +65,8 @@ CompareList.propTypes = {
       pushed_at: PropTypes.string,
     }),
   ).isRequired,
+  removeRepository: PropTypes.func.isRequired,
+  updateRepository: PropTypes.func.isRequired,
 };
 
 export default CompareList;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -16,21 +16,36 @@ export default class Main extends Component {
     repositories: [],
   };
 
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    this.setState({ loading: false, repositories: await this.getLocalRepositories() });
+  }
+
   handleAddRepository = async (e) => {
     e.preventDefault();
 
     this.setState({ loading: true });
 
+    const { repositoryInput, repositories } = this.state;
+
     try {
-      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+      const { data: repository } = await api.get(`/repos/${repositoryInput}`);
 
       repository.lastCommit = moment(repository.pushed_at).fromNow();
 
       this.setState({
         repositoryInput: '',
-        repositories: [...this.state.repositories, repository],
+        repositories: [...repositories, repository],
         repositoryError: false,
       });
+
+      const localRepositories = await this.getLocalRepositories();
+
+      await localStorage.setItem(
+        '@GitCompare:repositories',
+        JSON.stringify([...localRepositories, repository]),
+      );
     } catch (err) {
       this.setState({ repositoryError: true });
     } finally {
@@ -38,24 +53,58 @@ export default class Main extends Component {
     }
   };
 
+  getLocalRepositories = async () => JSON.parse(await localStorage.getItem('@GitCompare:repositories')) || [];
+
+  handleRemoveRepository = async (id) => {
+    const { repositories } = this.state;
+
+    const updateRepositories = repositories.filter(repository => repository.id !== id);
+
+    this.setState({ repositories: updateRepositories });
+
+    await localStorage.setItem('@GitCompare:repositories', JSON.stringify(updateRepositories));
+  };
+
+  handleUpdateRepository = async (id) => {
+    const { repositories } = this.state;
+
+    const repository = repositories.find(repo => repo.id === id);
+
+    try {
+      const { data } = await api.get(`/repos/${repository.full_name}`);
+
+      data.lastCommit = moment(data.pushed_at).fromNow();
+
+      this.setState({
+        repositoryError: false,
+        repositoryInput: '',
+        repositories: repositories.map(repo => (repo.id === data.id ? data : repo)),
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    }
+  };
+
   render() {
+    const {
+      repositories, repositoryInput, repositoryError, loading,
+    } = this.state;
+
     return (
       <Container>
         <img src={logo} alt="GitHub Compare" />
 
-        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
           <input
             type="text"
             placeholder="usuário/repositório"
-            value={this.state.repositoryInput}
+            value={repositoryInput}
             onChange={e => this.setState({ repositoryInput: e.target.value })}
           />
-          <button type="submit">
-            {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
-          </button>
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
         </Form>
 
-        <CompareList repositories={this.state.repositories} />
+        <CompareList repositories={repositories} updateRepository={this.handleUpdateRepository} removeRepository={this.handleRemoveRepository} />
       </Container>
     );
   }
